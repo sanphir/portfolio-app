2444841a54a51d53bb9e799db76218b692efd002
soldatkin <sanphir@yandex.ru>
2022-04-22 18:54:17 +0300

    Some styles fixes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,7 @@
 import { useLocation } from "react-router-dom"
 import { Link } from "react-router-dom"
 
-const NotFound = () => {
-  const location = useLocation();
-  console.log(`location=${JSON.stringify(location)}`);
+const NotFound = () => {  
   return (
     <div style={{
       display: "block",
@@ -20,17 +18,6 @@ const NotFound = () => {
         <h2 style={{ padding: "10px 20px", textAlign: "center", color: "rgb(25, 118, 210)" }}>Page not found!</h2>
 
         <h3 style={{ padding: "10px 20px", textAlign: "center", color: "rgb(25, 118, 210)" }}>Ooops! We can't find the page.</h3>
-        <div style={{
-          display: "flex",
-          justifyContent: "center"
-        }}>
-        <Link to={location?.pathname ?? "/home"} className="bacLink"
-        style={{
-          fontWeight: "bold",
-          color: "rgb(25, 118, 210)",
-          textDecorationColor: "rgb(25, 118, 210)",
-        }}> Back to {location?.pathname ?? "/home"}</Link>
-        </div>
       </div>
     </div>
   )
